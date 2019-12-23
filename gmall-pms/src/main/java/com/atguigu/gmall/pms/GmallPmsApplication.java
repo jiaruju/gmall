33bd5d11bc98bb447e91fb5b3ebc0ffea34bf574
jiaruju <upcjiarj@163.com>
2019-12-23 16:38:56 +0800

    项目构建完成.....

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
@@ -1,8 +1,23 @@
 package com.atguigu.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 1.配置整合dubbo
+ * 2.配置整合MybatisPlus
+ *
+ * logstash整合：
+ *      1.导jar包logstash-logback-encoder
+ *      2.导入日志配置logback-spring.xml
+ *      3.在kibana里面的discover建立好索引,就可以进行日志可视化检索
+ */
+
+@EnableDubbo
+@MapperScan(basePackages = "com.atguigu.gmall.pms.mapper")
 @SpringBootApplication
 public class GmallPmsApplication {
 
--- gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,42 @@
 package com.atguigu.gmall.pms;
 
+import com.atguigu.gmall.pms.entity.Brand;
+import com.atguigu.gmall.pms.entity.Product;
+import com.atguigu.gmall.pms.service.BrandService;
+import com.atguigu.gmall.pms.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class GmallPmsApplicationTests {
 
+    @Autowired
+    ProductService productService;
+
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+
+        Product product = productService.getById(1);
+        System.out.println(product.getName());
+    }
+
+    @Test
+    void test(){
+
+//        Brand brand = new Brand();
+//        brand.setName("哈哈哈");
+//        brandService.save(brand);
+//
+//        System.out.println("保存成功......");
+
+        Brand brand = brandService.getById(53);
+        System.out.println("查询成功...."+brand.getName());
     }
 
 }
